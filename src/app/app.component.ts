03fb30b4b7b7385fcedbb41e5544d2805c36e97e
Paul Wilker Landaeta Flores <paulwilkerlf@gmail.com>
2018-01-13 23:46:15 -0400

    verify if it's login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,26 @@
+import {HomePage} from '../pages/home/home';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { OAuthProvidersListPage } from '../pages/oauth/list/oauth-providers.list.page';
+import { OAuthService } from '../pages/oauth/oauth.service';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = OAuthProvidersListPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,private oauthService:OAuthService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      let env = this;
+      if(this.oauthService.isAuthorized()){
+        this.rootPage = HomePage
+        splashScreen.hide();
+      }
       statusBar.styleDefault();
       splashScreen.hide();
     });
